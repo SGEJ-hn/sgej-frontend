26ec5004aa2888afeb3e6f25781d866588f97fe3
abigailxdq <164611483+abigailxdq@users.noreply.github.com>
2026-07-27 16:56:21 -0600

    Mejora visual del componente Calendario

--- src/app/components/calendario/calendario.ts
+++ src/app/components/calendario/calendario.ts
@@ -5,6 +5,8 @@ import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
+import esLocale from '@fullcalendar/core/locales/es';
+
 @Component({
   selector: 'app-calendario',
   standalone: true,
@@ -14,9 +16,73 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 })
 export class Calendario {
 
-  calendarOptions: CalendarOptions = {
-    plugins: [dayGridPlugin],
-    initialView: 'dayGridMonth'
-  };
+ calendarOptions: CalendarOptions = {
+  plugins: [dayGridPlugin],
+  initialView: 'dayGridMonth',
+  locale: esLocale,
+  height: 'auto',
+  contentHeight: 'auto',
+
+  headerToolbar: {
+    left: 'title',
+    center: '',
+    right: 'prev,next'
+  },
+
+  eventDisplay: 'list-item',
+
+  events: [
+    {
+      start: '2026-07-07',
+      color: '#4CAF50'
+    },
+    {
+      start: '2026-07-10',
+      color: '#2196F3'
+    },
+    {
+      start: '2026-07-22',
+      color: '#FF9800'
+    },
+    {
+      start: '2026-07-30',
+      color: '#4CAF50'
+    }
+  ]
+};
+
+ngAfterViewInit() {
+
+  setTimeout(() => {
+
+    const dias = document.querySelectorAll('.fc-daygrid-day');
+
+    dias.forEach((dia: any) => {
+
+      if (dia.getAttribute('data-date') === '2026-07-07') {
+
+        const numero = dia.querySelector('.fc-daygrid-day-number');
+
+        if(numero){
+
+          numero.style.background = '#5b1f2b';
+          numero.style.color = 'white';
+          numero.style.width = '36px';
+          numero.style.height = '36px';
+          numero.style.display = 'flex';
+          numero.style.alignItems = 'center';
+          numero.style.justifyContent = 'center';
+          numero.style.borderRadius = '2px';
+          numero.style.margin = '6px';
+
+        }
+
+      }
+
+    });
+
+  });
+
+}
 
 }
