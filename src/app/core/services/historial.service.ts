3c89e0ce2edc52a7e95199ff86190908c1cd8ed9
Makzzzz <maxwellbaca13@gmail.com>
2026-08-20 14:50:57 -0700

    Feat: Correccion historial

--- src/app/core/services/historial.service.ts
+++ src/app/core/services/historial.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 export interface EventoHistorial {
   id_historial: string;
@@ -27,7 +28,7 @@ export interface RespuestaHistorial {
   providedIn: 'root'
 })
 export class HistorialService {
-  private apiUrl = 'http://localhost:3000/api/historial';
+  private apiUrl = `${environment.apiUrl}/historial`;
 
   constructor(private http: HttpClient) {}
 
